e6bd7c21def99f3458ab6c0c82fc3773d60dfcd1
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2020-11-24 17:38:51 -0700

    :sparkles: Adding connection to database

--- js/mychart.js
+++ js/mychart.js
@@ -5,12 +5,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'], //x axis
         datasets: [{
             label: 'My First dataset',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [0, 10, 5, 2, 20, 30, 45]  //y axis
         }]
     },
 
