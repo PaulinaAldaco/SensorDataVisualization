4bc44fff9f23f30a0fcd09c080b4925eb1b1f090
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2020-12-02 01:33:53 -0700

    :pencil: Fixed errors in potentiometers.php

--- data/potentiometers.php
+++ data/potentiometers.php
@@ -6,25 +6,25 @@
     //$sql = "SELECT sensor1Value FROM $dbname.$tbname WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1";
 
     //Get last values that are not null
-    $sql = "SELECT
-            (SELECT sensor1Value FROM $dbname.$tbname WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor1Value,
-            (SELECT sensor2Value FROM $dbname.$tbname WHERE sensor2Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor2Value,
-            (SELECT sensor3Value FROM $dbname.$tbname WHERE sensor3Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor3Value"
+    $sql = "SELECT (SELECT sensor1Value FROM potentiometers WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor1Value,
+	        (SELECT sensor2Value FROM potentiometers WHERE sensor2Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor2Value,
+	        (SELECT sensor3Value FROM potentiometers WHERE sensor3Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor3Value";
     
     // Perform Query
     $result = mysqli_query($conn,$sql);
 
     //Get int values
-    $row = mysqli_fetch_array($result, 'MYSQLI_ASSOC');
-    $pot1 = $row['sensor1Value'];
-    $pot2 = $row['sensor2Value'];
-    $pot3 = $row['sensor3Value'];
+    $result = $result->fetch_array();
+    $pot1 = intval($result[0]);
+    $pot2 = intval($result[1]);
+    $pot3 = intval($result[2]);
 
     //Add values to get final result
     $response = $pot1 + $pot2 + $pot3;
+    $response = strval($response);
     
     //Return final response
-    echo strval($response);
+    echo $response;
     
     //Close database connection
     mysqli_close($conn);
