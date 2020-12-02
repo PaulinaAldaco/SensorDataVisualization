fcca2024590eb558acd989098793e39ac0bf4a61
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2020-12-02 00:46:14 -0700

    :white_check_mark: Working on potentiometer test

--- data/testing-pot.php
+++ data/testing-pot.php
@@ -6,7 +6,9 @@
     //$sql = "SELECT sensor1Value FROM $dbname.$tbname WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1";
 
     //Get last values that are not null
-    $sql = "SELECT sensor1Value FROM $dbname.$tbname WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1";
+    $sql = "SELECT (SELECT sensor1Value FROM potentiometers WHERE sensor1Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor1Value,
+	        (SELECT sensor2Value FROM potentiometers WHERE sensor2Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor2Value,
+	        (SELECT sensor3Value FROM potentiometers WHERE sensor3Value IS NOT NULL ORDER BY ID DESC LIMIT 1) as sensor3Value";
     
     // Perform Query
     $result = mysqli_query($conn,$sql);
@@ -14,9 +16,12 @@
     //Get int values
     $result = $result->fetch_array();
     $pot1 = intval($result[0]);
+    $pot2 = intval($result[1]);
+    $pot3 = intval($result[2]);
 
     //Add values to get final result
-    $response = strval($pot1);
+    $response = $pot1 + $pot2 + $pot3;
+    $response = strval($response);
     
     //Return final response
     echo $response;
