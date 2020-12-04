38a931d303eda66ac5bd14bb5f3d7f79b356f5e3
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2020-12-04 14:02:48 -0700

    :lipstick: Final touches to landing page

--- js/myChart3.js
+++ js/myChart3.js
@@ -1,7 +1,7 @@
 
 // Create Chart with no data
 var ctx = document.getElementById('myChart3').getContext('2d');
-var myChart3 = new Chart(ctx, {
+var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: [],         // Labels are empty
@@ -9,8 +9,8 @@ var myChart3 = new Chart(ctx, {
             label: 'Humidity',
             data: [],       // Data is empty Adding it later, allows to see a pretty animation!
             fill: false,
-            borderColor: 'rgba(255, 99, 132, 1)',     
-            borderWidth: 1,
+            borderColor: '#c23669',     
+            borderWidth: 3,
             lineTension: 0
         }]
     },
